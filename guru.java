33e3c830d192b1f732b4ea584965b070c03bd0f4
dionrezkyoktaf24 <dion_oktafino_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:53:00 +0700

    Add files via upload

---
+++ guru.java
@@ -0,0 +1,22 @@
+//Guru Class
+public class guru {
+
+     int id;
+     Object mapel;
+     String nama;
+     String alamat;
+
+     //Constructor nama sama dengan mapel
+     public guru(){
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+
+     }
+     
+
+
+
+}
+
---
+++ tesguru.java
@@ -0,0 +1,85 @@
+//Driver Class
+public class tesguru {
+    
+    
+
+    public static void main(String[] args) {
+        // membuat obyek
+        guru Agus  = new guru();
+        guru Nasikin  = new guru();
+        guru Aulia  = new guru();
+        guru Firdausa  = new guru();
+        guru pashatania  = new guru();
+
+        // Pak Agus
+
+        Agus.id = 1;
+        Agus.nama = "Agus Hari Purwanto, S.Pd";
+        Agus.mapel = "Bahasa Indonesia";
+        Agus.alamat = "Kota Malang";
+
+        System.out.println("Ini Data Pak Agus");
+        System.out.println(Agus.id);
+        System.out.println(Agus.nama);
+        System.out.println(Agus.mapel);
+        System.out.println(Agus.alamat);
+
+        //Pak Nasikin
+
+        Nasikin.id = 2;
+        Nasikin.nama = "Ahmad Nasikin, M.Pd";
+        Nasikin.mapel = "Pendidikan Agama Islam";
+        Nasikin.alamat = "Kota Malang";
+
+        System.out.println("Ini Data Pak Nasikin");
+        System.out.println(Nasikin.id);
+        System.out.println(Nasikin.nama);
+        System.out.println(Nasikin.mapel);
+        System.out.println(Nasikin.alamat);
+
+        //Bu Aulia
+
+        Aulia.id = 3;
+        Aulia.nama = "Aulia Mas'adah, S.Kom";
+        Aulia.mapel = "Produktif RPL";
+        Aulia.alamat = "Kota Malang";
+
+        System.out.println("Ini Data Bu Aulia");
+        System.out.println(Aulia.id);
+        System.out.println(Aulia.nama);
+        System.out.println(Aulia.mapel);
+        System.out.println(Aulia.alamat);
+
+        //Pak Firdausa
+
+        Firdausa.id = 4;
+        Firdausa.nama = "Firdausa, S.Pd";
+        Firdausa.mapel = "Produktif RPL";
+        Firdausa.alamat = "Kota Malang";
+
+        System.out.println("Ini Data Pak Firdaus");
+        System.out.println(Firdausa.id);
+        System.out.println(Firdausa.nama);
+        System.out.println(Firdausa.mapel);
+        System.out.println(Firdausa.alamat);
+
+        //Bu Pashatania 
+
+        pashatania.id = 5;
+        pashatania.nama = "Pashatania Fitri Indah Lestari, S.Kom";
+        pashatania.mapel = "Program Keahlian RPL";
+        pashatania.alamat = "Kota Malang";
+
+        System.out.println("Ini Data Bu pashatania");
+        System.out.println(pashatania.id);
+        System.out.println(pashatania.nama);
+        System.out.println(pashatania.mapel);
+        System.out.println(pashatania.alamat);
+
+
+     
+
+
+        
+    }
+}
